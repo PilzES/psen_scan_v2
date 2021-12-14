14d1809a977bb46659eeb801125bff72aaa440df
Christian Döhn <c.doehn@pilz.de>
2021-12-14 15:50:50 +0100

    Apply changes to ScanRange

--- standalone/test/include/psen_scan_v2_standalone/util/integrationtest_helper.h
+++ standalone/test/include/psen_scan_v2_standalone/util/integrationtest_helper.h
@@ -72,8 +72,8 @@ static std::vector<double> generateIntensities(const unsigned int& num_elements,
 
 static data_conversion_layer::monitoring_frame::Message
 createValidMonitoringFrameMsg(const uint32_t scan_counter = 42,
-                              const util::TenthOfDegree start_angle = DEFAULT_SCAN_RANGE.getStart(),
-                              const util::TenthOfDegree end_angle = DEFAULT_SCAN_RANGE.getEnd(),
+                              const util::TenthOfDegree start_angle = DEFAULT_SCAN_RANGE.start(),
+                              const util::TenthOfDegree end_angle = DEFAULT_SCAN_RANGE.end(),
                               const uint8_t active_zoneset = 1)
 {
   const auto resolution{ util::TenthOfDegree(10) };
@@ -97,7 +97,7 @@ createValidMonitoringFrameMsg(const uint32_t scan_counter = 42,
 static data_conversion_layer::monitoring_frame::Message
 createValidMonitoringFrameMsgWithZoneset(const uint8_t active_zoneset)
 {
-  return createValidMonitoringFrameMsg(42, DEFAULT_SCAN_RANGE.getStart(), DEFAULT_SCAN_RANGE.getEnd(), active_zoneset);
+  return createValidMonitoringFrameMsg(42, DEFAULT_SCAN_RANGE.start(), DEFAULT_SCAN_RANGE.end(), active_zoneset);
 }
 
 std::vector<data_conversion_layer::monitoring_frame::Message>
@@ -116,9 +116,9 @@ createMonitoringFrameMsgsForScanRound(const uint32_t scan_counter, const std::si
   for (std::size_t i = 0; i < num_elements; ++i)
   {
     const util::TenthOfDegree start_angle =
-        (DEFAULT_SCAN_RANGE.getEnd() / static_cast<int>(num_elements)) * static_cast<int>(i);
+        (DEFAULT_SCAN_RANGE.end() / static_cast<int>(num_elements)) * static_cast<int>(i);
     const util::TenthOfDegree end_angle =
-        (DEFAULT_SCAN_RANGE.getEnd() / static_cast<int>(num_elements)) * static_cast<int>(i + 1);
+        (DEFAULT_SCAN_RANGE.end() / static_cast<int>(num_elements)) * static_cast<int>(i + 1);
     msgs.push_back(createValidMonitoringFrameMsg(scan_counter, start_angle, end_angle));
   }
   return msgs;
